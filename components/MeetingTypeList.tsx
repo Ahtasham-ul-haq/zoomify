298903ac6b23606cd7d367b3ef227f8a822d72d5
Ahtasham ul-Haq <ahtasham1802@gmail.com>
2024-09-18 23:50:19 +0500

    implement join meeting model

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -12,6 +12,7 @@ import { useToast } from "@/components/ui/use-toast";
 import { Textarea } from "./ui/textarea";
 
 import ReactDatePicker from "react-datepicker";
+import { Input } from "./ui/input";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -156,6 +157,21 @@ const MeetingTypeList = () => {
         buttonText="Start Meeting"
         handleClick={createMeeting}
       />
+
+      <MeetingModal
+        isOpen={meetingState === "isJoiningMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Type the link here"
+        className="text-center"
+        buttonText="Join Meeting"
+        handleClick={() => router.push(values.link)}
+      >
+        <Input 
+          placeholder="Meeting Link"
+          className="border-none bg-dark-3 focus-visibile:ring-0 focus-visible:ring-offset-0"
+          onChange={(e) => setValues({...values, link: e.target.value})}
+        />
+      </MeetingModal>
     </section>
   );
 };
