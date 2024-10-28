204fef39f23585fb0fb4aff12d796154a5114e0c
Ahtasham-ul-Haq <ahtasham1802@gmail.com>
2024-10-28 15:53:38 +0500

    fix homepage error

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -20,11 +20,7 @@ const Home = () => {
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="h-full flex flex-col justify-between px-4 py-6 md:px-5 md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
-<<<<<<< HEAD
-            Upcoming Meeting at 10:00 pm
-=======
-            Upcoming Meeting at: 12:00 PM
->>>>>>> 15a3a3afca33d0ffa30c848b06d2e252af649648
+            Upcoming meeting at 12:00 pm
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
